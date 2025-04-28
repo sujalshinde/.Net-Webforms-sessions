ae5dc121304730d04383d8d5b8f7fc95066a114f
Sujal Shinde <88922332+sujalshinde@users.noreply.github.com>
2025-04-28 13:00:15 +0530

    session 6

---
+++ session 6/literalexp.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace session_6
+{
+    public partial class literalexp : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            getPhotos();
+
+        }
+        protected void getPhotos()
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("select photos,thumb from Table_2  order by Srno DESC", con);
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            if (sdr.HasRows)
+            {
+                while (sdr.Read())
+                {
+                    LGallery.Text += "<div class='col-sm-12 col-md-4'><a class='lightbox' href='" + sdr.GetValue(0).ToString() + "'><img src='" + sdr.GetValue(1).ToString() + "' /></a></div>";
+                }
+            }
+
+        }
+    }
+}
