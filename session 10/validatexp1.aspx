ce22951903e3280ed048dfe4184a3282c31634b9
Sujal Shinde <88922332+sujalshinde@users.noreply.github.com>
2025-05-05 12:13:20 +0530

    session 10

---
+++ session 10/validatexp1.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="validatexp1.aspx.cs" Inherits="session_10.validatexp1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+    <div class="form-group">
+        <label for="email">Name:</label>
+        <asp:TextBox ID="txtname" runat="server" CssClass="form-control"></asp:TextBox>
+    </div>
+    <div class="form-group">
+        <label for="email">Password:</label>
+        <asp:TextBox ID="txtpassword1" runat="server" CssClass="form-control"></asp:TextBox>
+    </div>
+    <div class="form-group">
+        <label for="email">Confirm Password:</label>
+        <asp:TextBox ID="txtpassword2" runat="server" CssClass="form-control"></asp:TextBox>
+    </div>
+    <div class="form-group">
+        <label for="email">Age:</label>
+        <asp:TextBox ID="txtage" runat="server" CssClass="form-control"></asp:TextBox>
+    </div>
+    <div class="form-group">
+        <label for="email">Email:</label>
+        <asp:TextBox ID="txtemail" runat="server" CssClass="form-control"></asp:TextBox>
+    </div>
+    <div class="form-group">
+        <label for="email">Username:</label>
+        <asp:TextBox ID="txtusername" runat="server" CssClass="form-control"></asp:TextBox>
+    </div>
+    <asp:Button ID="btnsubmit" OnClick="btnsubmit_Click" runat="server" CssClass="btn btn-primary" Text="Submit" />
+   <hr />
+         <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Please Enter Name"
+        ControlToValidate="txtname"></asp:RequiredFieldValidator><br />
+         <asp:CompareValidator ID="CompareValidator1" runat="server" ErrorMessage="Password Not Matches"
+        ControlToValidate="txtpassword1" ControlToCompare="txtpassword2"></asp:CompareValidator><br />
+        <asp:RangeValidator ID="RangeValidator1" runat="server" ErrorMessage="Please enter age between 18-100"
+        ControlToValidate="txtage" MaximumValue="100" MinimumValue="18" Type="Integer"></asp:RangeValidator><br />
+        <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server"
+        ErrorMessage="Please Enter Valid email-id" ControlToValidate="txtemail"
+        ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*"></asp:RegularExpressionValidator><br />
+         <asp:CustomValidator ID="CustomValidator1" runat="server" OnServerValidate="UserCustomValidate"
+        ControlToValidate="txtusername" ErrorMessage="User ID should have atleast a capital, small and digit and should be greater than 5 and less than 26 letters" SetFocusOnError="True"></asp:CustomValidator>
+    <hr />
+    <asp:ValidationSummary ID="ValidationSummary1" runat="server" />
+   
+        </div>
+</asp:Content>
---
+++ session 10/validatexp1.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace session_10
+{
+    public partial class validatexp1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void UserCustomValidate(object source, ServerValidateEventArgs args)
+        {
+            string str = args.Value;
+            args.IsValid = false;
+            //checking for input length greater than 6 and less than 25 characters  
+            if (str.Length < 6 || str.Length > 25)
+            {
+                return;
+            }
+            //checking for a atleast a single capital letter  
+            bool capital = false;
+            foreach (char ch in str)
+            {
+                if (ch >= 'A' && ch <= 'Z')
+                {
+                    capital = true;
+                    break;
+                }
+            }
+            if (!capital)
+            {
+                return;
+            }
+            //checking for a atleast a single lower letter  
+            bool lower = false;
+            foreach (char ch in str)
+            {
+                if (ch >= 'a' && ch <= 'z')
+                {
+                    lower = true;
+                    break;
+                }
+            }
+            if (!lower)
+            {
+                return;
+            }
+            bool digit = false;
+            foreach (char ch in str)
+            {
+                if (ch >= '0' && ch <= '9')
+                {
+                    digit = true;
+                    break;
+                }
+            }
+            if (!digit)
+            {
+                return;
+            }
+            args.IsValid = true;
+
+           
+
+
+
+
+
+           
+
+            }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd1 = new SqlCommand("insert into Table_3 values (@name,@password, @ConfirmPassword,@age,@email,@username)", con);
+            cmd1.Parameters.AddWithValue("@name", txtname.Text);
+            cmd1.Parameters.AddWithValue("@password", txtpassword2.Text);
+            cmd1.Parameters.AddWithValue("@ConfirmPassword", txtpassword2.Text);
+            cmd1.Parameters.AddWithValue("@age", txtage.Text);
+            cmd1.Parameters.AddWithValue("@email", txtemail.Text);
+            cmd1.Parameters.AddWithValue("@username", txtusername.Text);
+
+
+
+
+
+            con.Open();
+            cmd1.ExecuteNonQuery();
+
+            txtname.Text = "";
+            txtpassword2.Text = "";
+            txtpassword2.Text = "";
+            txtage.Text = "";
+            txtemail.Text = "";
+            txtusername.Text = "";
+
+
+            Response.Write("<script>alert('Saved..!')</script>");
+        }
+
+    }
+}
+
+    
